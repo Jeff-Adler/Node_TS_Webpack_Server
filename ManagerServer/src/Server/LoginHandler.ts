3c2317769cd99ea87583071402de7e0ff3bd8471
Jeff Adler <JeffM.Adler@gmail.com>
2021-02-06 14:35:35 -0500

    retrieved username and password

--- ManagerServer/src/Server/LoginHandler.ts
+++ ManagerServer/src/Server/LoginHandler.ts
@@ -1,7 +1,33 @@
+import { throws } from 'assert';
 import { IncomingMessage, ServerResponse } from 'http';
 
 export class LoginHandler {
   constructor(private req: IncomingMessage, private res: ServerResponse) {}
 
-  public handleRequest(): void {}
+  public async handleRequest(): Promise<void> {
+    console.log('before getting Body');
+    const body = await this.getRequestBody();
+    console.log('request username: ' + body.username);
+    console.log('request password: ' + body.password);
+  }
+
+  private async getRequestBody(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      let body = '';
+      //possible responses to request
+      this.req.on('data', (data: string) => {
+        body += data;
+      });
+      this.req.on('end', () => {
+        try {
+          resolve(JSON.parse(body));
+        } catch (error) {
+          reject(error);
+        }
+      });
+      this.req.on('error', (error: any) => {
+        reject(error);
+      });
+    });
+  }
 }
